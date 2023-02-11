0a8ce32dea5311c6b8202fc93016960b758acc81
hoa-devv <vhoa21710@gmail.com>
2023-02-11 14:11:41 +0700

    delete

--- Project CGV/cgv-web-master/src/main/java/com/project/khoaluan/dao/PhongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.khoaluan.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.khoaluan.model.Phong;
-
-public interface PhongRepository  extends CrudRepository<Phong, Integer>{
-	Phong findById(int id);
-}
